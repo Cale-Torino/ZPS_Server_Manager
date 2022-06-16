1ae029b5513f1d41b404b8c3a8990e2abc9f6d5b
Cale-Torino <cale@techrad.co.za>
2022-06-16 10:15:00 +0200

    Added show logs form

---
+++ ZPS_Server_Manager/Core/ZPS_Server_Manager/Forms/ShowLogsForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ZPS_Server_Manager
+{
+    partial class ShowLogsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(431, 500);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // ShowLogsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(455, 524);
+            this.Controls.Add(this.richTextBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "ShowLogsForm";
+            this.Text = "ShowLogsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+    }
+}
