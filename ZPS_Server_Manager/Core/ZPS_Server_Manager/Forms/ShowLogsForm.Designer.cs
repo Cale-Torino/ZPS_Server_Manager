db772d51a485dfad97c2a6459fbbf7119f096241
Cale-Torino <cale@techrad.co.za>
2022-06-17 10:08:42 +0200

    Added close and open button to show logs form .net6

--- ZPS_Server_Manager/Core/ZPS_Server_Manager/Forms/ShowLogsForm.Designer.cs
+++ ZPS_Server_Manager/Core/ZPS_Server_Manager/Forms/ShowLogsForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.OpenFilebutton = new System.Windows.Forms.Button();
+            this.Closebutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -39,16 +41,40 @@
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(431, 500);
+            this.richTextBox1.Size = new System.Drawing.Size(830, 500);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            // 
+            // OpenFilebutton
+            // 
+            this.OpenFilebutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(7)))), ((int)(((byte)(7)))));
+            this.OpenFilebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenFilebutton.Location = new System.Drawing.Point(486, 518);
+            this.OpenFilebutton.Name = "OpenFilebutton";
+            this.OpenFilebutton.Size = new System.Drawing.Size(175, 34);
+            this.OpenFilebutton.TabIndex = 3;
+            this.OpenFilebutton.Text = "Open File";
+            this.OpenFilebutton.UseVisualStyleBackColor = true;
+            // 
+            // Closebutton
+            // 
+            this.Closebutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(7)))), ((int)(((byte)(7)))));
+            this.Closebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Closebutton.Location = new System.Drawing.Point(667, 518);
+            this.Closebutton.Name = "Closebutton";
+            this.Closebutton.Size = new System.Drawing.Size(175, 34);
+            this.Closebutton.TabIndex = 4;
+            this.Closebutton.Text = "Close";
+            this.Closebutton.UseVisualStyleBackColor = true;
             // 
             // ShowLogsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(455, 524);
+            this.ClientSize = new System.Drawing.Size(854, 567);
+            this.Controls.Add(this.Closebutton);
+            this.Controls.Add(this.OpenFilebutton);
             this.Controls.Add(this.richTextBox1);
             this.ForeColor = System.Drawing.Color.White;
             this.Name = "ShowLogsForm";
@@ -60,5 +86,7 @@
         #endregion
 
         private RichTextBox richTextBox1;
+        private Button OpenFilebutton;
+        private Button Closebutton;
     }
 }
