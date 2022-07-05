5700bcc6ecf42756ed9a8b76417386df771b2a9e
Cale-Torino <cale@techrad.co.za>
2022-07-05 11:52:17 +0200

    added updates form to core version

---
+++ ZPS_Server_Manager/Core/ZPS_Server_Manager/Forms/CheckForUpdatesForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace ZPS_Server_Manager
+{
+    partial class CheckForUpdatesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CheckForUpdatesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(598, 192);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CheckForUpdatesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Update Check";
+            this.Load += new System.EventHandler(this.CheckForUpdatesForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
